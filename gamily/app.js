89eba318db7fafe67dff6436155f9cab4ae9273c
Frankpeter Ani <frankpeterani@gmail.com>
2020-07-23 20:35:13 +0100

    Added animations with gsap

--- gamily/app.js
+++ gamily/app.js
@@ -2,32 +2,32 @@ gsap.from(".welcome", { duration: 1, x: "-100vw", ease: "easeIn.out" });
 gsap.from(".gamily", {
   duration: 2,
   opacity: 0,
-  delay: 0,
-  ease: "bounce.in",
+  delay: 2.8,
+  ease: "power2.in",
 });
 gsap.from(".peer", {
   duration: 2,
   x: "100vw",
-  delay: 2,
+  delay: 0.5,
   ease: "sine.out",
 });
 gsap.from(".lorem", {
   duration: 2,
   x: "-100vw",
-  delay: 1.5,
+  delay: 1,
   ease: "expo.out",
 });
 gsap.from(".get-started", {
   duration: 2,
   opacity: 0,
-  delay: 2,
+  delay: 4.5,
   ease: "bounce.in",
   stagger: 2,
 });
 gsap.from(".pad", {
-  duration: 2,
+  duration: 3,
   y: "-100vh",
   opacity: 0,
-  delay: 1,
+  delay: 1.2,
   ease: "rough.in",
 });
